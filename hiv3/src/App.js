eefa774b467595e4c7a85ce550f157e3d0b3e02f
Aneal <akuverji@scu.edu>
2024-01-28 14:53:59 -0800

    modified App.js

--- hiv3/src/App.js
+++ hiv3/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
-
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.js';
+/*import Profile from './pages/Profile.js';
+import ManageCamera from './pages/ManageCamera.js';'
+import ModifyPermissions from './pages/ModifyPermissions.js';
+import LiveViewfrom './pages/LiveView.js';
+import ViewData from './pages/data.js';
+*/
 function App() {
+
+  useEffect(() => {
+    document.title = 'Hiv3';
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+    </Routes>
+    </Router>
   );
 }
 
