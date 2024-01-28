0c063c17fdf3d9b4082147ca3afbc465ae3320be
Aneal <akuverji@scu.edu>
2024-01-28 14:56:59 -0800

    fixed with branch

---
+++ Hiv3/src/App.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.js';
+/*import Profile from './pages/Profile.js';
+import ManageCamera from './pages/ManageCamera.js';'
+import ModifyPermissions from './pages/ModifyPermissions.js';
+import LiveViewfrom './pages/LiveView.js';
+import ViewData from './pages/data.js';
+*/
+function App() {
+
+  useEffect(() => {
+    document.title = 'Hiv3';
+  }, []);
+
+
+  return (
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
