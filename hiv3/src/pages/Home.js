898ea682f1f36e9679e38da803e6d633f51cd7df
Aneal <akuverji@scu.edu>
2024-02-01 08:54:16 -0800

    edited to be committed

--- hiv3/src/pages/Home.js
+++ hiv3/src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from "react"; 
-
+import homebutton from ".././media/homebutton.png";
+import logo from ".././media/Hive Logo.png";
+import pfp from ".././media/user-pfp.png";
+//import {useGoogleLogin} from "@react-oauth/google";
 
 const Home = () => {
+/*    const googSignIn=useGoogleLogin({
+        onSuccess:(res)=>{
+            console.log("res",res);
+            alert("Login Successful");
+        },
+    });*/
     return (
-        <div class = "background">
-           <></> 
+        <div class="homepage">
+            <div class = "topbar">
+                <img src={homebutton} className="homebutton" alt="homebutton" />
+                <img src={logo} className="center" alt="Hive Logo" />
+                <img src={pfp} className="right"alt ="pfp" />
+            </div>
         </div>
     
     );
