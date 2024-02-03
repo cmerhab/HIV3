613979ed2a780645864ee74ec3da260a2e3eed18
Aneal <akuverji@scu.edu>
2024-02-03 15:06:25 -0800

    made some progress

--- hiv3/src/pages/Home.js
+++ hiv3/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import homebutton from ".././media/homebutton.png";
 import logo from ".././media/Hive Logo.png";
 import pfp from ".././media/user-pfp.png";
+import ".././pages/Home.css";
 //import {useGoogleLogin} from "@react-oauth/google";
 
 const Home = () => {
@@ -13,13 +14,22 @@ const Home = () => {
     });*/
     return (
         <div class="homepage">
-            <div class = "topbar">
+            <div class = "topbar-container">
                 <img src={homebutton} className="homebutton" alt="homebutton" />
                 <img src={logo} className="center" alt="Hive Logo" />
-                <img src={pfp} className="right"alt ="pfp" />
+                <img src={pfp} className="right" alt ="pfp" />
+            </div>
+            <div class="bar"></div>
+            <div class="left-box">
+                <div class="camera-info">
+                    <p1>Camera Name:</p1>
+                    <p1>Local Time:</p1>
+                    <p1>Local Temperature:</p1>
+                </div>
             </div>
         </div>
-    
+
+
     );
 
 };
