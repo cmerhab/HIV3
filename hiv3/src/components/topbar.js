37ae95ca9d1fd6bd26a7884118cca4022023e008
cmerhab <131628830+cmerhab@users.noreply.github.com>
2024-02-07 14:26:32 -0800

    yo

--- hiv3/src/components/topbar.js
+++ hiv3/src/components/topbar.js
@@ -3,11 +3,14 @@ import Home from ".././media/homebutton.png"
 import Logo from ".././media/Hive Logo.png"
 import Pfp from ".././media/user-pfp.png"
 import ".././styles/Topbar.css";
+import { Navigate } from "react-router-dom";
 const Topbar = () => {
     return (
     <div>
     <div class = "topbar-container">
-                <img src={Home} className="homebutton" alt="homebutton" />
+                <button class ="HomeButtonclick">
+                    <img src={Home} className="homebutton" alt="homebutton" />
+                </button>
                 <img src={Logo} className="center" alt="Hive Logo" />
                 <img src={Pfp} className="right" alt ="pfp" />
     </div>
