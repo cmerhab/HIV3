51c3d268939e427be2bfb73496f83717e90b13c6
cmerhab <131628830+cmerhab@users.noreply.github.com>
2024-02-20 19:38:11 -0800

    Signin v2 page

--- src/components/signin.js
+++ src/components/signin.js
@@ -27,14 +27,30 @@ const SignIn = () => {
 
     return (
     <div className = 'signinposition'>
-        <div className='signinbox'>
-            <h1>HIV3 Monitor</h1>
-            <img src={Logo} className="HiveLogo" alt="Hive Logo" />
-            <div className='googlebutton'>
-                <GoogleButton onClick={handleGoogleSignIn} />
+        <div className='leftside'>
+            <div classname ="alltext">
+                <h1 className = 'headertext'>Welcome To The HIV3!</h1>
+                <p className = 'bodytext'>Description of Project</p>
+            </div>
+        </div>
+
+        <div className='rightside'>
+            <div className='signinbox'>
+                <img src={Logo} className="HiveLogo" alt="Hive Logo" /> 
+             
+                <div className="logintext">
+                <h1>HIV3 Monitor</h1>
+                <br />
+                <div className='googlebutton'>
+                    <GoogleButton onClick={handleGoogleSignIn} />
+                </div>
+                </div>
             </div>
         </div>
     </div>
     )
 }
 export default SignIn;
+
+
+           
