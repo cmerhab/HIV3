ad1510d276bad9f4262ccbcf7238bfb8810ff961
cmerhab <131628830+cmerhab@users.noreply.github.com>
2024-03-05 17:45:54 -0800

    Added an existing users list

--- src/components/adminpopup.js
+++ src/components/adminpopup.js
@@ -5,11 +5,39 @@ import {UserAuth} from '../context/AuthContext'
 import roles from '../context/roles.json'
 
 const AdminPopup = ({setAdminPopup}) => {
+    const organizedData = {};
+    const role = roles.Role;
+
+    if(!organizedData[role]){
+        organizedData[role] = [];
+    }
+
+    roles.members.forEach(member => {
+        organizedData[role].push(member.aemail);
+    });
+
+
    return (
     <div className ="adminpopup">
-        <div classname ="popupheader">
-            <h1>Add User</h1>
-            <button onClick={()=>setAdminPopup(false)}> X </button>
+        <div className ="popupheader">
+            <div className = "addusersec">
+                <h1>Add User</h1>
+            </div>
+            <div className ="existing-role-section">
+                <h1>Existing Users</h1>
+                    <select multiple className="existinglist">
+                        {Object.keys(organizedData).map((role, index) => (
+                            <optgroup label ={`Role: ${role}`} key={index}> 
+                                {organizedData[role].map((email, index) => (
+                                    <option key = {index} value ={email} className="emails" disabled >{`Email: ${email}`}</option>
+                                ))}
+                            </optgroup>
+                        ))}
+                    </select>
+            </div>
+            <div className="exitbutton">
+                <button onClick={()=>setAdminPopup(false)}> X </button>
+            </div>
         </div>
     </div>
    )
