58dbc793d3996423ff683d9837615c9746624f30
cmerhab <131628830+cmerhab@users.noreply.github.com>
2024-03-07 05:49:29 -0800

    Demote and Promote user functionality added!

--- src/components/demoteuser.js
+++ src/components/demoteuser.js
@@ -1,13 +1,94 @@
 import React, {useState, useEffect} from 'react'
 import {Navigate} from 'react-router-dom'
 import {UserAuth} from '../context/AuthContext'
-import currentroles from '../context/roles.json'
+import ReactSearchBox from "react-search-box";
 
 const DemoteUser = () => {
+    const [guestdata, setGuestData] = useState([]);
+    const [guestselect, setGuestSelect] = useState();
+    const [guestbutton, setGuestbutton] = useState(false);
+    const fetchGuestEmails = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/Roles?Role=Guest');
+            const [guestRole] = await response.json();
 
+            if(guestRole && guestRole.members)
+            {
+                const guestEmails = guestRole.members.map(member => ({
+                    key: member.userid,
+                    value: member.aemail,
+                }));
+                setGuestData(guestEmails);
+            }
+        } catch (error) {
+            console.error('Failed to fetch guest list', error);
+        }
+    };
+
+    const handleSelect = (record) => {
+        console.log(record);
+        setGuestSelect(record);
+        setGuestbutton(true);
+    }
+
+    const handleBanUser = async () => {
+        if(!guestselect)
+            return;
+        const userId = guestselect.item.key;
+        const userEmail = guestselect.item.value;
+
+        try {
+            //Fetching the Banned Role
+            const responseBanned = await fetch('http://localhost:3001/Roles?Role=Banned');
+            const [bannedRole] = await responseBanned.json();
+            const updatedBannedMembers = [...bannedRole.members, { "aemail": userEmail, "userid": userId}]; 
+            
+            //Adding new Member to Banned Role
+            await fetch(`http://localhost:3001/Roles/${bannedRole.id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ members: updatedBannedMembers }),
+            });
+
+            //Fetching filtered guest list(without the one moved to banned)
+            const responseGuest = await fetch('http://localhost:3001/Roles?Role=Guest');
+            const [guestRole] = await responseGuest.json();
+            const filteredGuestMembers = guestRole.members.filter(member => member.userid !== userId);
+
+            await fetch(`http://localhost:3001/Roles/${guestRole.id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ members: filteredGuestMembers }),
+            });
+            console.log(`${userEmail} Banned.`)
+        } catch (error)
+        {
+            console.error('Failed to ban user', error);
+        }
+        setGuestbutton(false);
+    }
+    useEffect(() => {
+        fetchGuestEmails();
+    }, [])
+
+   
     return(
         <div>
-            <p>DEMOTE</p>
+            <br />
+            <p>Guest Users</p>
+            <ReactSearchBox
+                placeholder='Demote User'
+                value = "Doe"
+                data = {guestdata}
+                onSelect={handleSelect}
+
+            />
+            {guestselect && guestbutton && (
+                <div className="demote">
+                    <p>Ban {guestselect.item.value}?</p> 
+                    <button onClick={handleBanUser}>Ban User</button>
+                </div>
+            )}
         </div>
     )
 };
--- src/components/promoteuser.js
+++ src/components/promoteuser.js
@@ -1,13 +1,95 @@
 import React, {useState, useEffect} from 'react'
 import {Navigate} from 'react-router-dom'
 import {UserAuth} from '../context/AuthContext'
-import currentroles from '../context/roles.json'
+import ReactSearchBox from "react-search-box";
 
 const PromoteUser = () => {
+    const [banneddata, setBannedData] = useState([]);
+    const [bannedselect, setBannedSelect] = useState();
+    const [bannedbutton, setBannedButton] = useState(false);
+
+    const fetchBannedEmails = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/Roles?Role=Banned');
+            const [bannedRole] = await response.json();
+
+            if(bannedRole && bannedRole.members)
+            {
+                const bannedEmails = bannedRole.members.map(member => ({
+                    key: member.userid,
+                    value: member.aemail,
+                }));
+                setBannedData(bannedEmails);
+            }
+        } catch (error) {
+            console.error('Failed to fetch ban list', error);
+        }
+    }
+
+    const handleUnbannedUser = async () => {
+        if(!bannedselect)
+            return;
+        const userId = bannedselect.item.key;
+        const userEmail = bannedselect.item.value;
+
+        try {
+            //Fetching the Guest Role
+            const responseGuest = await fetch('http://localhost:3001/Roles?Role=Guest');
+            const [guestRole] = await responseGuest.json();
+            const updatedGuestMembers = [...guestRole.members, { "aemail": userEmail, "userid": userId}]; 
+
+             //Adding new Member to Guest Role
+             await fetch(`http://localhost:3001/Roles/${guestRole.id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ members: updatedGuestMembers }),
+            });
+
+            //Fetching filtered banned list(without the one moved to banned)
+            const responseBanned = await fetch('http://localhost:3001/Roles?Role=Banned');
+            const [bannedRole] = await responseBanned.json();
+            const filteredBannedMembers = bannedRole.members.filter(member => member.userid !== userId);
+
+            //replacing old banned list with new banned list
+            await fetch(`http://localhost:3001/Roles/${bannedRole.id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ members: filteredBannedMembers }),
+            });
+            console.log(`${userEmail} Unbanned.`)
+        } catch (error) {
+            console.error('Failed to unban user', error);
+        }
+
+        setBannedButton(false);
+    }
+
+    const handleSelect = (record) => {
+        console.log(record);
+        setBannedSelect(record);
+        setBannedButton(true);
+    }
+
+    useEffect(() => {
+        fetchBannedEmails();
+    }, [])
 
     return(
         <div>
-            <p>PROMOTE</p>
+            <br />
+            <p>Banned Users</p>
+            <ReactSearchBox
+                placeholder='Promote User'
+                value = "Doe"
+                data = {banneddata}
+                onSelect={handleSelect}
+            />
+            {bannedselect && bannedbutton && (
+                <div className="promote">
+                    <p>Unban {bannedselect.item.value}?</p> 
+                    <button onClick={handleUnbannedUser}>Unban User</button>
+                </div>
+            )}
         </div>
     )
 };
